567b03c2d423865d613e00ec2ce7dcb8004fd948
Eddy Ashton <v-edasht@microsoft.com>
2019-07-23 12:01:12 +0100

    Pull out old tables/commands

--- src/apps/tess/tess.cpp
+++ src/apps/tess/tess.cpp
@@ -21,18 +21,6 @@ namespace tess
     using RolesMap = ccfapp::Store::Map<ccf::CallerId, Roles>;
     RolesMap& user_roles;
 
-    static constexpr ccf::ValueId REPO_VAL_ID = ccf::ValueIds::END_ID + 1;
-    using RepoID = ccf::Value;
-    using ReposMap = ccfapp::Store::Map<std::string, RepoID>;
-    ReposMap& repo_ids;
-
-    // TODO: JSON is general for now,
-    static constexpr ccf::ValueId BUILD_VAL_ID = REPO_VAL_ID + 1;
-    using BuildID = ccf::Value;
-    using Build = std::pair<RepoID, nlohmann::json>;
-    using BuildsMap = ccfapp::Store::Map<BuildID, Build>;
-    BuildsMap& builds;
-
     struct BranchData
     {};
 
@@ -72,38 +60,10 @@ namespace tess
       return roles;
     }
 
-    RepoID get_repo(ccf::Store::Tx& tx, const std::string& name)
-    {
-      auto repos_view = tx.get_view(repo_ids);
-      auto repo_it = repos_view->get(name);
-      if (!repo_it.has_value())
-      {
-        throw std::runtime_error(fmt::format("No repo named {}", name));
-      }
-
-      return repo_it.value();
-    }
-
-    inline ccf::Value get_next_id(ccf::Store::Tx& tx, ccf::ValueId id)
-    {
-      auto values_view = tx.get_view(network.values);
-      try
-      {
-        return ccf::get_next_id(values_view, id);
-      }
-      catch (const std::logic_error& e)
-      {
-        values_view->put(id, 1);
-        return 0;
-      }
-    }
-
     TessApp(ccf::NetworkTables& nwt, ccf::AbstractNotifier& notifier) :
       UserRpcFrontend(*nwt.tables),
       network(nwt),
       user_roles(tables.create<RolesMap>("user-roles")),
-      repo_ids(tables.create<ReposMap>("repo-ids")),
-      builds(tables.create<BuildsMap>("builds")),
       branches(tables.create<BranchesMap>("branches"))
     {
       auto roles_get = [this](RequestArgs& args) {
@@ -143,99 +103,6 @@ namespace tess
         return jsonrpc::success(roles);
       };
       install("ROLES_ADD", roles_add, Write);
-
-      auto repos_add = [this](RequestArgs& args) {
-        auto repo_name_it = args.params.find("name");
-        if (repo_name_it == args.params.end())
-          return jsonrpc::error(
-            jsonrpc::StandardErrorCodes::INVALID_PARAMS, "Missing param: name");
-
-        RepoID next_repo_id = get_next_id(args.tx, REPO_VAL_ID);
-
-        auto repos_view = args.tx.get_view(repo_ids);
-        const auto name = repo_name_it->get<std::string>();
-        const auto existing = repos_view->get(name);
-        if (existing.has_value())
-          return jsonrpc::error(
-            jsonrpc::StandardErrorCodes::INTERNAL_ERROR,
-            fmt::format("Repo with name {} already exists", name));
-
-        repos_view->put(name, next_repo_id);
-
-        auto j = nlohmann::json::object();
-        j["name"] = name;
-        j["internal_id"] = next_repo_id;
-
-        return jsonrpc::success(j);
-      };
-      install("REPOS_ADD", repos_add, Write);
-
-      auto repos_list = [this](RequestArgs& args) {
-        auto names = nlohmann::json::array();
-        auto repos_view = args.tx.get_view(repo_ids);
-        repos_view->foreach([&names](const std::string& s, auto) {
-          names.push_back(s);
-          return true;
-        });
-
-        return jsonrpc::success(names);
-      };
-      install("REPOS_LIST", repos_list, Read);
-
-      auto builds_add = [this](RequestArgs& args) {
-        auto repo_name_it = args.params.find("repo");
-        if (repo_name_it == args.params.end())
-          return jsonrpc::error(
-            jsonrpc::StandardErrorCodes::INVALID_PARAMS, "Missing param: repo");
-
-        auto build_info_it = args.params.find("build_info");
-        if (build_info_it == args.params.end())
-          return jsonrpc::error(
-            jsonrpc::StandardErrorCodes::INVALID_PARAMS,
-            "Missing param: build_info");
-
-        const auto repo_name = repo_name_it->get<std::string>();
-        const auto repo_id = get_repo(args.tx, repo_name);
-
-        const auto build_info = *build_info_it;
-
-        const auto build_id = get_next_id(args.tx, BUILD_VAL_ID);
-
-        auto builds_view = args.tx.get_view(builds);
-        if (builds_view->get(build_id).has_value())
-          return jsonrpc::error(
-            jsonrpc::StandardErrorCodes::INTERNAL_ERROR,
-            fmt::format("Build ID {} is already in use", build_id));
-
-        builds_view->put(build_id, std::make_pair(repo_id, build_info));
-
-        return jsonrpc::success(fmt::format("Build #{} stored", build_id));
-      };
-      install("BUILDS_ADD", builds_add, Write);
-
-      auto builds_get = [this](RequestArgs& args) {
-        auto build_id_it = args.params.find("build_id");
-        if (build_id_it == args.params.end())
-          return jsonrpc::error(
-            jsonrpc::StandardErrorCodes::INVALID_PARAMS,
-            "Missing param: build_index");
-
-        const auto build_id = build_id_it->get<size_t>();
-
-        auto builds_view = args.tx.get_view(builds);
-        const auto build = builds_view->get(build_id);
-        if (!build.has_value())
-          return jsonrpc::error(
-            jsonrpc::StandardErrorCodes::INTERNAL_ERROR,
-            fmt::format("Build ID {} is not valid", build_id));
-
-        auto j = nlohmann::json::object();
-        j["repo"] = build->first;
-        j["info"] = build->second;
-
-        return jsonrpc::success(j);
-      };
-      install("BUILDS_GET", builds_get, Read);
 
       auto create_release_branch = [this](RequestArgs& args) {
         CreateReleaseBranch::In in =
