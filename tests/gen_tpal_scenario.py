0dcd48c5813fef42deb50526035069a925476690
Amaury Chamayou <amchamay@microsoft.com>
2020-01-08 10:29:18 +0000

    split tpal bench

--- tests/gen_tpal_scenario.py
+++ tests/gen_tpal_scenario.py
@@ -1,14 +1,16 @@
 import json
 import uuid
+import hashlib
 
 if __name__ == "__main__":
     txs = []
     for _ in range(500):
+        h = hashlib.sha256(uuid.uuid4().bytes).hexdigest()
         txs.append({
             "method": "LOG_record",
             "params": {
                 "id": 0,
-                "msg": f"Unique message: {uuid.uuid4()}"
+                "msg": f"Copyright (c) Microsoft Corporation. All rights reserved. {h}"
             }
         })
     print(json.dumps({"transactions": txs}))
